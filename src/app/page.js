f30b424fd502c60bc1ed97419599fff7cb509ee7
Bruno Visconti <65882022+visavisconti@users.noreply.github.com>
2025-07-20 17:47:43 +0200

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import PoemOfTheDay from '../components/PoemOfTheDay2';
+import PoemOfTheDay from '../components/PoemOfTheDay';
 
 export default function Home() {
   return (
